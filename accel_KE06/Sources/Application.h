20974f40acfcf1efe0fe1e88b42b5464f2d8cf8d
Alejandro <perez.alejandrodaniel@gmail.com>
2016-06-25 18:07:29 -0300

    Added two more projects to test the features of the KE06 board.

---
+++ accel_KE06/Sources/Application.h
@@ -0,0 +1,29 @@
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#include "PE_Types.h"
+#include "PE_LDD.h"
+
+typedef struct {
+  LDD_TDeviceData *handle;  /* LDD device handle */
+  volatile uint8_t isSent;  /* this will be set to 1 once the block has been sent */
+  uint8_t rxChar; 			/* single character buffer for receiving chars */
+  uint8_t (*rxPutFct)(uint8_t); /* callback to put received character into buffer */
+} UART_Desc;
+
+typedef struct {
+  LDD_TDeviceData *handle;  /* LDD device handle */
+  volatile uint8_t isSent;  /* this will be set to 1 once the block has been sent */
+  uint8_t rxChar; 			/* single character buffer for receiving chars */
+  uint8_t (*rxPutFct)(uint8_t); /* callback to put received character into buffer */
+} CAN_Desc;
+
+typedef struct {
+  volatile bool dataReceivedFlg; /* set to TRUE by the interrupt if we have received data */
+  volatile bool dataTransmittedFlg; /* set to TRUE by the interrupt if we have set data */
+  LDD_TDeviceData *handle; /* pointer to the device handle */
+} MMA8451_TDataState;
+
+void APP_Run(void);
+
+#endif /* APPLICATION_H_ */
